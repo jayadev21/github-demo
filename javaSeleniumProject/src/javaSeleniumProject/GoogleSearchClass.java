415b75eeb925cea2474f5e95923ddb4d3f7b9a6f
jayadev21 <jdevreddy2020@gmail.com>
2020-11-21 14:45:43 -0500

    Google search a new project

--- javaSeleniumProject/src/javaSeleniumProject/GoogleSearchClass.java
+++ javaSeleniumProject/src/javaSeleniumProject/GoogleSearchClass.java
@@ -2,19 +2,10 @@ package javaSeleniumProject;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class webLoginUsers {
+public class GoogleSearchClass {
 
 	public static void main(String[] args) {
-		
-		//new FirefoxDriver();
-	   /*System.setProperty("webdriver.gecko.driver", "/Users/Reddy/eclipse-workspace/geckodriver");	
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://salesforce.com");
-		System.out.println(driver.getTitle());
-		System.out.println(driver.getCurrentUrl());
-		driver.quit();*/
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/Reddy/eclipse-workspace/chromedriver");
 		WebDriver driver= new ChromeDriver();
