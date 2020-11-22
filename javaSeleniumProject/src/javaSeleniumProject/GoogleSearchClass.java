05fa0b596f39946c44772bf8e08f37f36de18bc2
jayadev21 <jdevreddy2020@gmail.com>
2020-11-21 19:50:24 -0500

    This is Google Login Script

--- javaSeleniumProject/src/javaSeleniumProject/GoogleSearchClass.java
+++ javaSeleniumProject/src/javaSeleniumProject/GoogleSearchClass.java
@@ -1,5 +1,6 @@
 package javaSeleniumProject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,8 +11,16 @@ public class GoogleSearchClass {
 		System.setProperty("webdriver.chrome.driver", "/Users/Reddy/eclipse-workspace/chromedriver");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://google.com");
-		System.out.println(driver.getTitle());
+		//System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
+		driver.findElement(By.xpath("//a[@id='gb_70']")).click();
+		//System.out.println(driver.findElement(By.xpath("//*[@jsname='l4eHX']")).getText());
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("jdevreddy2020@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]")).click();
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Hyderabad@00");
+		driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/div[2]")).click();
+		
+		
 		//driver.quit();
 		
 	}
